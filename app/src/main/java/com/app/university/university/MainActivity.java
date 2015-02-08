38df9263f3bf3aa55c913a670d5a9a3e84f8c7b1
acstep <acstep@gmail.com>
2015-02-09 00:10:41 +0800

    init

---
+++ app/src/main/java/com/app/university/university/MainActivity.java
@@ -0,0 +1,102 @@
+package com.app.university.university;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.DisplayMetrics;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.astuetz.PagerSlidingTabStrip;
+
+
+public class MainActivity extends FragmentActivity {
+
+    private ChatFragment ChatFragment;
+    private CChatFragment CChatFragment;
+
+    private DisplayMetrics dm;
+    private PagerSlidingTabStrip tabs;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        dm = getResources().getDisplayMetrics();
+        ViewPager pager = (ViewPager) findViewById(R.id.pager);
+        tabs = (PagerSlidingTabStrip) findViewById(R.id.tabs);
+
+
+        tabs.setShouldExpand(true);
+        tabs.setIndicatorColor(Color.parseColor("#45c01a"));
+        pager.setAdapter(new MyPagerAdapter(getSupportFragmentManager()));
+        tabs.setViewPager(pager);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public class MyPagerAdapter extends FragmentPagerAdapter {
+
+        public MyPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        private final String[] titles = { "聊天", "發     現" };
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return titles[position];
+        }
+
+        @Override
+        public int getCount() {
+            return titles.length;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    ChatFragment = new ChatFragment();
+
+                    return ChatFragment;
+                case 1:
+
+                    CChatFragment = new CChatFragment();
+
+                    return CChatFragment;
+
+                default:
+                    return null;
+            }
+        }
+
+    }
+}
---
+++ app/src/main/java/com/app/university/university/Schedule.java
@@ -0,0 +1,37 @@
+package com.app.university.university;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+/**
+ * Created by matt on 2015/2/6.
+ */
+class ChatFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT);
+        FrameLayout fl = new FrameLayout(getActivity());
+        fl.setLayoutParams(params);
+        DisplayMetrics dm = getResources().getDisplayMetrics();
+        final int margin = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 8, dm);
+        TextView v = new TextView(getActivity());
+        params.setMargins(margin, margin, margin, margin);
+        v.setLayoutParams(params);
+        v.setLayoutParams(params);
+        v.setGravity(Gravity.CENTER);
+        v.setText("聊天界面");
+        v.setTextSize((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 12, dm));
+        fl.addView(v);
+        return fl;
+    }
+}
