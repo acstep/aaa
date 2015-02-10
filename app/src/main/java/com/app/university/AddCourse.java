9d9ec847251249b4fe268b7c2f58232c548f0d6e
acstep <acstep@gmail.com>
2015-02-10 16:11:50 +0800

    add files

--- app/src/main/java/com/app/university/AddCourse.java
+++ app/src/main/java/com/app/university/AddCourse.java
@@ -1,5 +1,6 @@
 package com.app.university;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -7,21 +8,25 @@ import android.view.View;
 import android.widget.Button;
 
 
-public class CreateAccountActivity extends Activity {
+public class AddCourse extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_account);
-        Button createButton = (Button)findViewById(R.id.btn_createaccount);
-        createButton.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_add_course);
+        ActionBar actionBar = getActionBar();
+        actionBar.hide();
+
+        Button loginButton = (Button)findViewById(R.id.btn_create_course_done);
+        loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View arg0) {
-                Intent intent = new Intent(arg0.getContext(), AddCourse.class);
+                Intent intent = new Intent(arg0.getContext(), MainActivity.class);
                 startActivity(intent);
                 finish();
             }
         });
+
     }
 
 
